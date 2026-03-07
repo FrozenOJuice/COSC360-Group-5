3f3ec7d1b2ab1d569e7f9494c9993ca0e8312120
FrozenOJuice <79174905+FrozenOJuice@users.noreply.github.com>
2026-03-07 15:34:52 -0800

    feat: add user repository functions

--- backend/src/repositories/userRepository.js
+++ backend/src/repositories/userRepository.js
@@ -1,5 +1,23 @@
 import User from "../models/User.js";
 
+export async function findByEmail(email) {
+    return User.findOne({ email });
+}
+
+export async function findById(userId) {
+    return User.findById(userId);
+}
+
+export async function createUser(userData) {
+    return User.create(userData);
+}
+
+export async function saveUser(user) {
+    return user.save();
+}
+
+
+
 export async function setRefreshTokenHash(userId, refreshTokenHash) {
     return User.findByIdAndUpdate(
         userId,
