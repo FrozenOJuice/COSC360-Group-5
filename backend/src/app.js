39a96b6898aa43ee223a9e958f68f67d0f9090f9
FrozenOJuice <79174905+FrozenOJuice@users.noreply.github.com>
2026-03-09 12:28:17 -0700

    CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./routers/authRouter.js";
 
 import { notFound } from "./middleware/notFound.js";
@@ -7,6 +8,10 @@ import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
